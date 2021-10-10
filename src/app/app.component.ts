b56ac6739635cd3e2387cbc473cca50ffb8fb383
Brian Breedlove <brianbreedlove@Brians-MacBook-Air.local>
2021-10-10 16:35:51 -0700

    cards showing and complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      userName: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'SNowy Mountain',
+      userName: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'I got lost on this mountain and I like mittens',
+    },
+    {
+      title: 'biking',
+      userName: 'nature',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I saw this neat bike today while biking',
+    },
+    {
+      title: 'SNowy Mountain',
+      userName: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'I got lost on this mountain and I like mittens',
+    },
+  ];
 }
